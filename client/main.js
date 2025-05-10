82b17791f1f312391f8e7aeb126cd3146f088ffe
nvh12 <154247185+nvh12@users.noreply.github.com>
2025-05-10 22:40:11 +0700

    Project setup

---
+++ client/main.js
@@ -0,0 +1,16 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function creatWindow() {
+    const window = new BrowserWindow({
+        width: 1280,
+        heigt: 720,
+        resizable: true,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            nodeIntegration: false,
+            contextIsolation: true,
+            sandbox: true
+        }
+    })
+}
